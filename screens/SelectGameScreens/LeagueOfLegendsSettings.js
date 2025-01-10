0fb6cef404db7403de9f6bf816922311b8dfe0e9
Faddy52 <lucas11091109@gmail.com>
2025-01-10 16:55:54 +0200

    Improved Visuals

--- screens/SelectGameScreens/LeagueOfLegendsSettings.js
+++ screens/SelectGameScreens/LeagueOfLegendsSettings.js
@@ -193,7 +193,7 @@ const styles = StyleSheet.create({
     fontSize: 22,
     color: '#fff',
     fontWeight: 'bold',
-    marginBottom: 20,
+    marginBottom: 100,
     textAlign: 'center',
   },
   pickerContainer: {
